0c802b2048633b4ff48e724cecf71ae269153279
Nguyen Quoc Huy <nqh.d3v@gmail.com>
2023-02-23 10:35:02 +0700

    Update API handler

--- src/pages/api/strava-webhook.ts
+++ src/pages/api/strava-webhook.ts
@@ -1,20 +1,40 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  isErr?: boolean,
-  err?: any;
-  data?: any;
-}
-
 export default function handler (
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
   try {
-    console.log('Got strava-webhook callback:', req)
-    res.send({ data: req })
+    if (req.method === 'GET') {
+      // Your verify token. Should be a random string.
+      const VERIFY_TOKEN = process.env.STRAVA_VERIFY_TOKEN
+      // Parses the query params
+      const mode = req.query['hub.mode']
+      const token = req.query['hub.verify_token']
+      const challenge = req.query['hub.challenge']
+      // Checks if a token and mode is in the query string of the request
+      if (mode && token) {
+        // Verifies that the mode and token sent are valid
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+          // Responds with the challenge token from the request
+          console.log('WEBHOOK VERIFIED')
+          res.json({ 'hub.challenge': challenge })
+          return
+        }
+        // Responds with '403 Forbidden' if verify tokens do not match
+      }
+      res.status(403).json({ err: 'Forbidden' })
+      return
+    }
+    if (req.method === 'POST') {
+      console.log('WEBHOOK RECEIVED DATA:', req.query, req.body)
+      res.status(200).send('EVENT_RECEIVED')
+      return
+    }
+    res.status(404).send({ err: 'Invalid request' })
   } catch (err) {
-    res.status(400).send({ err })
+    console.error('Error when handling request:', req.method, err)
+    res.status(404).send({ err: 'Invalid request!' })
   }
 }
